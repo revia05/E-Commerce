4ddb68fab92dd89eb17ba6cf3ae71ad4b99e5489
ROSHAN KUMAR <147585559+revia05@users.noreply.github.com>
2025-06-23 21:12:56 +0530

    Add files via upload

---
+++ src/data/dummyProducts.js
@@ -0,0 +1,8 @@
+export const dummyProducts = [
+    { id: 1, name: "Multigrain Atta", price: 120, image: "/img/multi.jpg" },
+    { id: 2, name: "Besan Flour", price: 90, image: "/img/Laxmi-Besan-Chickpea-Flour_b175f891-e632-4aec-990b-57825615f3a2.bf98feae9e4d12f9a30d7bd0b1ac1117.avif" },
+    { id: 3, name: "Rice Flour", price: 70, image: "/img/rice.webp" },
+    { id: 4, name: "Maida", price: 50, image: "/img/maida.jpg" },
+    { id: 5, name: "Wheat Flour", price: 100, image: "/img/wheat.jpg" }
+  ];
+  
