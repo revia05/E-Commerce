1fb8c944811dda9bb35b20715ebc0ce094c7c04e
vibhortiwary <163341245+vibhortiwary@users.noreply.github.com>
2025-06-29 01:49:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,36 @@ import CartPage from "./components/CartPage";
 import WishlistPage from "./components/WishlistPage";
 import AboutPage from "./components/AboutPage";
 import ContactPage from "./components/ContactPage";
+import LoginPage from "./components/LoginPage";
+import SignupPage from "./components/SignupPage";
 import { CartProvider } from "./context/CartContext";
 import { WishlistProvider } from "./context/WishlistContext";
+import { AuthProvider } from "./context/AuthContext";
 import "./styles/main.css";
 
 const App = () => (
-  <CartProvider>
-    <WishlistProvider>
-      <Router>
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<SearchBar />} />
-            <Route path="/about" element={<AboutPage />} />    
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/wishlist" element={<WishlistPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </Router>
-    </WishlistProvider>
-  </CartProvider>
+  <AuthProvider>
+    <CartProvider>
+      <WishlistProvider>
+        <Router>
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<SearchBar />} />
+              <Route path="/about" element={<AboutPage />} />    
+              <Route path="/contact" element={<ContactPage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/wishlist" element={<WishlistPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </Router>
+      </WishlistProvider>
+    </CartProvider>
+  </AuthProvider>
 );
 
 export default App;
+
